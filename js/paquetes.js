73e3f123686f7c825647f67a1b705e44f8bb1476
skemerer <solangekemerer@gmail.com>
2023-10-15 23:10:00 -0300

    correccion proyecto 2

---
+++ js/paquetes.js
@@ -0,0 +1,8 @@
+const paquetes = [
+    { id: 1, tipo: "Paquete", nombre: "Transporte Redondo y Acceso", precio: 450, foto: "./media/1.jpg" },
+    { id: 2, tipo: "Paquete", nombre: "Transporte Redondo", precio: 200, foto: "./media/2.jpg" },
+    { id: 3, tipo: "Paquete", nombre: "Acceso a Six Flags", precio: 300, foto: "./media/3.jpg" },
+    { id: 4, tipo: "Paquete", nombre: "Transporte Sencillo", precio: 120, foto: "./media/4.jpg" },
+    { id: 5, tipo: "Dulce", nombre: "Refresco", precio: 25, foto: "./media/5.jpg" },
+    { id: 6, tipo: "Dulce", nombre: "Sandwich", precio: 25, foto: "./media/6.jpg" }
+];
